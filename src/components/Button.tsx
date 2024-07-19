604acc03170436dcd9f76446a72690155246d832
Dikshantw <dikshantw11@gmail.com>
2024-07-20 01:16:33 +0530

    added button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+const Button = ({ label, icon }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none bg-[#ff6452] rounded-full text-white border-[#ff6452]">
+      {label}
+      <img src={icon} alt="label" className="ml-2 rounded-full w-5 h-5" />
+    </button>
+  );
+};
+
+export default Button;
