e8b8e44fe7d3118dadabd19b43b0df685c9a43ae
Dikshantw <dikshantw11@gmail.com>
2024-07-20 18:43:46 +0530

    conditional style changes for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,25 @@
 interface ButtonProps {
   label: string;
   icon?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
 }
-const Button: React.FC<ButtonProps> = ({ label, icon }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  icon,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none bg-[#ff6452] rounded-full text-white border-[#ff6452]">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : `bg-[#ff6452] text-white border-[#ff6452]`
+      }  rounded-full`}
+    >
       {label}
       {icon && (
         <img src={icon} alt="label" className="ml-2 rounded-full w-5 h-5" />
