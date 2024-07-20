1325256649cf40a78b81dcdd3af8157eb3dc819c
Dikshantw <dikshantw11@gmail.com>
2024-07-20 17:11:37 +0530

    added conditional image rendering for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 interface ButtonProps {
   label: string;
-  icon: string;
+  icon?: string;
 }
 const Button: React.FC<ButtonProps> = ({ label, icon }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none bg-[#ff6452] rounded-full text-white border-[#ff6452]">
       {label}
-      <img src={icon} alt="label" className="ml-2 rounded-full w-5 h-5" />
+      {icon && (
+        <img src={icon} alt="label" className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
   );
 };
